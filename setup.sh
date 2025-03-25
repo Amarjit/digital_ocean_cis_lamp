2e2f07c932b11bc3f350e4f38011b4fa9e593360
Amarjit Bharath <hi@amarjit.uk>
2025-03-25 04:38:30 +0000

    Better messaging.

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ DOMAIN=$1
 EMAIL=$2
 
 # Update system. Non-interactive and upgrade all packages regardless of custom versions.
-echo -e "\n 🟩  Updating system..."
+echo -e "\n 🟩  Updating system
 DEBIAN_FRONTEND=noninteractive apt update > /dev/null 2>&1 && sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade -y > /dev/null 2>&1
 
 # Set execute permissions.
 chmod +x setup/setup_apache.sh setup/setup_php.sh setup/setup_website.sh setup/setup_ssl.sh
 
 # Run setup scripts.
-echo -e "\n 🟩  Running setup scripts..."
+echo -e "\n 🟩  Running setup scripts"
 ./setup/setup_apache.sh
 ./setup/setup_php.sh
 
