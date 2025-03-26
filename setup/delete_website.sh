a4aff36ffa863128d2bb4d64d5721b6e0fd4a447
Amarjit Bharath <hi@amarjit.uk>
2025-03-26 10:35:44 +0000

    Added individual checks.

--- setup/delete_website.sh
+++ setup/delete_website.sh
@@ -27,5 +27,33 @@ rm -f $VHOST_2
 rm -f $VHOST_3
 rm -f $VHOST_4
 
+# Check files and folders have been erased
+if [[ -d "$WWW" ]]; then
+    echo "\n 🟥  Directory $WWW has not been deleted."
+fi
+if [[ -d "$SSL" ]]; then
+    echo "\n 🟥  Directory $SSL has not been deleted."
+fi
+if [[ -f "$VHOST_1" ]]; then
+    echo "\n 🟥  File $VHOST_1 has not been deleted."
+fi
+if [[ -f "$VHOST_2" ]]; then
+    echo "\n 🟥  File $VHOST_2 has not been deleted."
+fi
+if [[ -f "$VHOST_3" ]]; then
+    echo "\n 🟥  File $VHOST_3 has not been deleted."
+fi
+if [[ -f "$VHOST_4" ]]; then
+    echo "\n 🟥  File $VHOST_4 has not been deleted."
+fi
+
+# Exit if any files or directories still exist
+if [[ -d "$WWW" ]] || [[ -d "$SSL" ]] || [[ -f "$VHOST_1" ]] || [[ -f "$VHOST_2" ]] || [[ -f "$VHOST_3" ]] || [[ -f "$VHOST_4" ]]; then
+    echo "🟥 Some files or directories have not been fully erased. Retry."
+    exit 1
+fi
+
 # Reload Apache
 systemctl reload apache2
+
+echo "\n ✅  Website $DOMAIN has been deleted."
