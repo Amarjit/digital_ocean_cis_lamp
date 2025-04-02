fb7aa302e495cbd6ec9fc69b7ddbe7f69ead3455
Amarjit Bharath <hi@amarjit.uk>
2025-04-02 23:25:03 +0100

    Created env file generation so all paths exist in 1 place. Repeated paths throughout files. delet website script updated to use new env.

--- setup/delete_website.sh
+++ setup/delete_website.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # Params
 DOMAIN=$1
+DOMAIN_ENV_PATH="/var/www/$DOMAIN/.env"
 
 # Ensure DOMAIN is set
 if [[ -z "$DOMAIN" ]]; then
@@ -7,120 +10,118 @@ if [[ -z "$DOMAIN" ]]; then
     exit 1
 fi
 
-VHOST_1="/etc/apache2/sites-enabled/002-$DOMAIN.conf"
-VHOST_2="/etc/apache2/sites-enabled/002-$DOMAIN-le-ssl.conf"
-VHOST_3="/etc/apache2/sites-available/002-$DOMAIN.conf"
-VHOST_4="/etc/apache2/sites-available/002-$DOMAIN-le-ssl.conf"
-VHOST_5="/etc/apache2/sites-available/002-$DOMAIN-selfsigned.conf"
-VHOST_6="/etc/apache2/sites-enabled/002-$DOMAIN-selfsigned.conf"
-WWW="/var/www/$DOMAIN"
-SSL="/etc/letsencrypt/live/$DOMAIN"
-SSL_SELFCERT="/etc/ssl/$DOMAIN"
-CRON_FILE="/etc/cron.d/${DOMAIN}__blue-green-deploy"
+# Check if environment file exists
+if [[ ! -f "$DOMAIN_ENV_PATH" ]]; then
+    echo "🟥 Environment file .env not found. Aborting."
+    exit 1
+fi
+
+# Load environment variables
+source "$DOMAIN_ENV_PATH"
 
 # Check what exists before deleting
-if [[ ! -d "$WWW" && ! -d "$SSL" && ! -f "$VHOST_1" && ! -f "$VHOST_2" && ! -f "$VHOST_3" && ! -f "$VHOST_4" && ! -f "$VHOST_5" && ! -f "$VHOST_6" && ! -f "$CRON_FILE" ]]; then
+if [[ ! -d "$WWW_PATH" && ! -d "$LETSENCRYPT_DOMAIN_PATH" && ! -f "$VHOST_ENABLED_DOMAIN_FILE_PATH" && ! -f "$VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH" && ! -f "$VHOST_AVAILABLE_DOMAIN_FILE_PATH" && ! -f "$VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH" && ! -f "$VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH" && ! -f "$VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH" && ! -f "$CRON_BLUEGREEN_DEPLOY_FILE" ]]; then
     echo "🟨 Nothing to delete. No files or directories exist for $DOMAIN"
     exit 0
 fi
 
-if [[ -d "$WWW" ]]; then
-    echo "🟩 Directory $WWW exists and will be deleted."
+if [[ -d "$WWW_PATH" ]]; then
+    echo "🟩 Directory $WWW_PATH exists and will be deleted."
 else
-    echo "🟥 Directory $WWW does not exist."
+    echo "🟥 Directory $WWW_PATH does not exist."
 fi
-if [[ -d "$SSL" ]]; then
-    echo "🟩 Directory $SSL exists and will be deleted."
+if [[ -d "$LETSENCRYPT_DOMAIN_PATH" ]]; then
+    echo "🟩 Directory $LETSENCRYPT_DOMAIN_PATH exists and will be deleted."
 else
-    echo "🟥 Directory $SSL does not exist."
+    echo "🟥 Directory $LETSENCRYPT_DOMAIN_PATH does not exist."
 fi
-if [[ -d "$SSL_SELFCERT" ]]; then
-    echo "🟩 Directory $SSL_SELFCERT exists and will be deleted."
+if [[ -d "$SELFCERT_DOMAIN_PATH" ]]; then
+    echo "🟩 Directory $SELFCERT_DOMAIN_PATH exists and will be deleted."
 else
-    echo "🟥 Directory $SSL_SELFCERT does not exist."
+    echo "🟥 Directory $SELFCERT_DOMAIN_PATH does not exist."
 fi
-if [[ -f "$VHOST_1" ]]; then
-    echo "🟩 File $VHOST_1 exists and will be deleted."
+if [[ -f "$VHOST_ENABLED_DOMAIN_FILE_PATH" ]]; then
+    echo "🟩 File $VHOST_ENABLED_DOMAIN_FILE_PATH exists and will be deleted."
 else
-    echo "🟥 File $VHOST_1 does not exist."
+    echo "🟥 File $VHOST_ENABLED_DOMAIN_FILE_PATH does not exist."
 fi
-if [[ -f "$VHOST_2" ]]; then
-    echo "🟩 File $VHOST_2 exists and will be deleted."
+if [[ -f "$VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH" ]]; then
+    echo "🟩 File $VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH exists and will be deleted."
 else
-    echo "🟥 File $VHOST_2 does not exist."
+    echo "🟥 File $VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH does not exist."
 fi
-if [[ -f "$VHOST_3" ]]; then
-    echo "🟩 File $VHOST_3 exists and will be deleted."
+if [[ -f "$VHOST_AVAILABLE_DOMAIN_FILE_PATH" ]]; then
+    echo "🟩 File $VHOST_AVAILABLE_DOMAIN_FILE_PATH exists and will be deleted."
 else
-    echo "🟥 File $VHOST_3 does not exist."
+    echo "🟥 File $VHOST_AVAILABLE_DOMAIN_FILE_PATH does not exist."
 fi
-if [[ -f "$VHOST_4" ]]; then
-    echo "🟩 File $VHOST_4 exists and will be deleted."
+if [[ -f "$VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH" ]]; then
+    echo "🟩 File $VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH exists and will be deleted."
 else
-    echo "🟥 File $VHOST_4 does not exist."
+    echo "🟥 File $VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH does not exist."
 fi
-if [[ -f "$VHOST_5" ]]; then
-    echo "🟩 File $VHOST_5 exists and will be deleted."
+if [[ -f "$VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH" ]]; then
+    echo "🟩 File $VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH exists and will be deleted."
 else
-    echo "🟥 File $VHOST_5 does not exist."
+    echo "🟥 File $VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH does not exist."
 fi
-if [[ -f "$VHOST_6" ]]; then
-    echo "🟩 File $VHOST_6 exists and will be deleted."
+if [[ -f "$VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH" ]]; then
+    echo "🟩 File $VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH exists and will be deleted."
 else
-    echo "🟥 File $VHOST_6 does not exist."
+    echo "🟥 File $VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH does not exist."
 fi
-if [[ -f "$CRON_FILE" ]]; then
-    echo "🟩 File $CRON_FILE exists and will be deleted."
+if [[ -f "$CRON_BLUEGREEN_DEPLOY_FILE" ]]; then
+    echo "🟩 File $CRON_BLUEGREEN_DEPLOY_FILE exists and will be deleted."
 else
-    echo "🟥 File $CRON_FILE does not exist."
+    echo "🟥 File $CRON_BLUEGREEN_DEPLOY_FILE does not exist."
 fi
 
-rm -rf $WWW
-rm -rf $SSL
-rm -rf $SSL_SELFCERT
-rm -f $VHOST_1
-rm -f $VHOST_2
-rm -f $VHOST_3
-rm -f $VHOST_4
-rm -f $VHOST_5
-rm -f $VHOST_6
-rm -f $CRON_FILE
+rm -rf $WWW_PATH
+rm -rf $LETSENCRYPT_DOMAIN_PATH
+rm -rf $SELFCERT_DOMAIN_PATH
+rm -f $VHOST_ENABLED_DOMAIN_FILE_PATH
+rm -f $VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH
+rm -f $VHOST_AVAILABLE_DOMAIN_FILE_PATH
+rm -f $VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH
+rm -f $VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH
+rm -f $VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH
+rm -f $CRON_BLUEGREEN_DEPLOY_FILE
 
 # Check files and folders have been erased
-if [[ -d "$WWW" ]]; then
-    echo -e "\n 🟥  Directory $WWW has not been deleted."
+if [[ -d "$WWW_PATH" ]]; then
+    echo -e "\n 🟥  Directory $WWW_PATH has not been deleted."
 fi
-if [[ -d "$SSL" ]]; then
-    echo -e "\n 🟥  Directory $SSL has not been deleted."
+if [[ -d "$LETSENCRYPT_DOMAIN_PATH" ]]; then
+    echo -e "\n 🟥  Directory $LETSENCRYPT_DOMAIN_PATH has not been deleted."
 fi
-if [[ -d "$SSL_SELFCERT" ]]; then
-    echo -e "\n 🟥  Directory $SSL_SELFCERT has not been deleted."
+if [[ -d "$SELFCERT_DOMAIN_PATH" ]]; then
+    echo -e "\n 🟥  Directory $SELFCERT_DOMAIN_PATH has not been deleted."
 fi
-if [[ -f "$VHOST_1" ]]; then
-    echo -e "\n 🟥  File $VHOST_1 has not been deleted."
+if [[ -f "$VHOST_ENABLED_DOMAIN_FILE_PATH" ]]; then
+    echo -e "\n 🟥  File $VHOST_ENABLED_DOMAIN_FILE_PATH has not been deleted."
 fi
-if [[ -f "$VHOST_2" ]]; then
-    echo -e "\n 🟥  File $VHOST_2 has not been deleted."
+if [[ -f "$VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH" ]]; then
+    echo -e "\n 🟥  File $VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH has not been deleted."
 fi
-if [[ -f "$VHOST_3" ]]; then
-   echo -e "\n 🟥  File $VHOST_3 has not been deleted."
+if [[ -f "$VHOST_AVAILABLE_DOMAIN_FILE_PATH" ]]; then
+   echo -e "\n 🟥  File $VHOST_AVAILABLE_DOMAIN_FILE_PATH has not been deleted."
 fi
-if [[ -f "$VHOST_4" ]]; then
-    echo -e "\n 🟥  File $VHOST_4 has not been deleted."
+if [[ -f "$VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH" ]]; then
+    echo -e "\n 🟥  File $VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH has not been deleted."
 fi
-if [[ -f "$VHOST_5" ]]; then
-    echo -e "\n 🟥  File $VHOST_5 has not been deleted."
+if [[ -f "$VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH" ]]; then
+    echo -e "\n 🟥  File $VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH has not been deleted."
 fi
-if [[ -f "$VHOST_6" ]]; then
-    echo -e "\n 🟥  File $VHOST_6 has not been deleted."
+if [[ -f "$VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH" ]]; then
+    echo -e "\n 🟥  File $VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH has not been deleted."
 fi
-if [[ -f "$CRON_FILE" ]]; then
-    echo -e "\n 🟥  File $CRON_FILE has not been deleted."
+if [[ -f "$CRON_BLUEGREEN_DEPLOY_FILE" ]]; then
+    echo -e "\n 🟥  File $CRON_BLUEGREEN_DEPLOY_FILE has not been deleted."
 fi
 
 # Exit if any files or directories still exist
-if [[ -d "$WWW" ]] || [[ -d "$SSL" ]] || [[ -d "$SSL_SELFCERT" ]] || [[ -f "$VHOST_1" ]] || [[ -f "$VHOST_2" ]] || [[ -f "$VHOST_3" ]] || [[ -f "$VHOST_4" ]] || [[ -f "$VHOST_5" ]] || [[ -f "$VHOST_6" ]] || [[ -f "$CRON_FILE" ]]; then
-    echo "🟥 Some files or directories have not been fully erased. Retry."
+if [[ -d "$WWW_PATH" ]] || [[ -d "$LETSENCRYPT_DOMAIN_PATH" ]] || [[ -d "$SELFCERT_DOMAIN_PATH" ]] || [[ -f "$VHOST_ENABLED_DOMAIN_FILE_PATH" ]] || [[ -f "$VHOST_ENABLED_DOMAIN_LETSENCRYPT_FILE_PATH" ]] || [[ -f "$VHOST_AVAILABLE_DOMAIN_FILE_PATH" ]] || [[ -f "$VHOST_AVAILABLE_DOMAIN_LETSENCRYPT_FILE_PATH" ]] || [[ -f "$VHOST_AVAILABLE_DOMAIN_SELFCERT_FILE_PATH" ]] || [[ -f "$VHOST_ENABLED_DOMAIN_SELFCERT_FILE_PATH" ]] || [[ -f "$CRON_BLUEGREEN_DEPLOY_FILE" ]]; then
+    echo "🟥  Some files or directories have not been fully erased. Retry."
     exit 1
 fi
 
