946c6e3187c8cb581bf2cbb8a07b85374e30ed25
Amarjit Bharath <hi@amarjit.uk>
2025-04-02 01:35:12 +0100

    Added new self cert to delete script.

--- setup/delete_website.sh
+++ setup/delete_website.sh
@@ -11,13 +11,15 @@ VHOST_1="/etc/apache2/sites-enabled/002-$DOMAIN.conf"
 VHOST_2="/etc/apache2/sites-enabled/002-$DOMAIN-le-ssl.conf"
 VHOST_3="/etc/apache2/sites-available/002-$DOMAIN.conf"
 VHOST_4="/etc/apache2/sites-available/002-$DOMAIN-le-ssl.conf"
+VHOST_5="/etc/apache2/sites-available/002-$DOMAIN-selfsigned.conf"
+VHOST_6="/etc/apache2/sites-enabled/002-$DOMAIN-selfsigned.conf"
 WWW="/var/www/$DOMAIN"
 SSL="/etc/letsencrypt/live/$DOMAIN"
 SSL_SELFCERT="/etc/ssl/$DOMAIN"
 CRON_FILE="/etc/cron.d/${DOMAIN}__blue-green-deploy"
 
 # Check what exists before deleting
-if [[ ! -d "$WWW" && ! -d "$SSL" && ! -f "$VHOST_1" && ! -f "$VHOST_2" && ! -f "$VHOST_3" && ! -f "$VHOST_4" && ! -f "$CRON_FILE" ]]; then
+if [[ ! -d "$WWW" && ! -d "$SSL" && ! -f "$VHOST_1" && ! -f "$VHOST_2" && ! -f "$VHOST_3" && ! -f "$VHOST_4" && ! -f "$VHOST_5" && ! -f "$VHOST_6" && ! -f "$CRON_FILE" ]]; then
     echo "🟨 Nothing to delete. No files or directories exist for $DOMAIN"
     exit 0
 fi
@@ -57,6 +59,16 @@ if [[ -f "$VHOST_4" ]]; then
 else
     echo "🟥 File $VHOST_4 does not exist."
 fi
+if [[ -f "$VHOST_5" ]]; then
+    echo "🟩 File $VHOST_5 exists and will be deleted."
+else
+    echo "🟥 File $VHOST_5 does not exist."
+fi
+if [[ -f "$VHOST_6" ]]; then
+    echo "🟩 File $VHOST_6 exists and will be deleted."
+else
+    echo "🟥 File $VHOST_6 does not exist."
+fi
 if [[ -f "$CRON_FILE" ]]; then
     echo "🟩 File $CRON_FILE exists and will be deleted."
 else
@@ -70,6 +82,8 @@ rm -f $VHOST_1
 rm -f $VHOST_2
 rm -f $VHOST_3
 rm -f $VHOST_4
+rm -f $VHOST_5
+rm -f $VHOST_6
 rm -f $CRON_FILE
 
 # Check files and folders have been erased
@@ -94,12 +108,18 @@ fi
 if [[ -f "$VHOST_4" ]]; then
     echo -e "\n 🟥  File $VHOST_4 has not been deleted."
 fi
+if [[ -f "$VHOST_5" ]]; then
+    echo -e "\n 🟥  File $VHOST_5 has not been deleted."
+fi
+if [[ -f "$VHOST_6" ]]; then
+    echo -e "\n 🟥  File $VHOST_6 has not been deleted."
+fi
 if [[ -f "$CRON_FILE" ]]; then
     echo -e "\n 🟥  File $CRON_FILE has not been deleted."
 fi
 
 # Exit if any files or directories still exist
-if [[ -d "$WWW" ]] || [[ -d "$SSL" ]] || [[ -d "$SSL_SELFCERT" ]] || [[ -f "$VHOST_1" ]] || [[ -f "$VHOST_2" ]] || [[ -f "$VHOST_3" ]] || [[ -f "$VHOST_4" ]] || [[ -f "$CRON_FILE" ]]; then
+if [[ -d "$WWW" ]] || [[ -d "$SSL" ]] || [[ -d "$SSL_SELFCERT" ]] || [[ -f "$VHOST_1" ]] || [[ -f "$VHOST_2" ]] || [[ -f "$VHOST_3" ]] || [[ -f "$VHOST_4" ]] || [[ -f "$VHOST_5" ]] || [[ -f "$VHOST_6" ]] || [[ -f "$CRON_FILE" ]]; then
     echo "🟥 Some files or directories have not been fully erased. Retry."
     exit 1
 fi
